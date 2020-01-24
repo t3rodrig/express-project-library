79a0db0d04b0129a84710f6b31815fc45f29cb36
Tonalli <tonalli@xanum.uam.mx>
2020-01-24 12:03:05 -0600

    Add books view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router  = express.Router();
 
+const Book = require('../models/book');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/books', (req, res, next) => {
+  Book.find()
+    .then(books => {
+      console.log('Retrived books from DB:', books);
+      res.render('books', {books});
+    })
+    .catch(err => console.log('Error while getting the books from DB:', err));
 });
 
 module.exports = router;
