acad440d6e3ee90c5fa6cd03c4ee6fba5973f14e
Tonalli <tonalli@xanum.uam.mx>
2020-01-24 23:17:31 -0600

    Add author route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 const Book = require('../models/book');
+const Author = require('../models/author');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -54,12 +55,34 @@ router.post('/books/edit', (req, res, next) => {
 
 router.get('/books/:bookId', (req, res, next) => {
   let bookId = req.params.bookId;
-
+  if (!/^[0-9a-fA-F]{24}$/.test(bookId)) { 
+    return res.status(404).render('not-found');
+  }
   Book.findById(bookId)
+    .populate('author')
     .then(book => {
+      if (!book) {
+        return res.status(404).render('not-found');
+      }
       res.render('book-details', {book});
+      console.log(book);
     })
     .catch(err => console.log('Error while retrieving book details: ', err));
+});
+
+router.get('/authors/add', (req, res, next) => {
+  res.render('author-add');
+});
+
+router.post('/authors/add', (req, res, next) => {
+  const data = { name, lastName, nationality, birthday, pictureUrl } = req.body;
+  Author.create(data)
+  .then(author => {
+    res.redirect('/books');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 });
 
 module.exports = router;
