4348f56c604dd3122813c48b06e7ca49d8cf34de
Tonalli <tonalli@xanum.uam.mx>
2020-01-27 12:06:22 -0600

    Add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -86,4 +86,8 @@ router.post('/login', (req, res, next) => {
 
 });
 
+router.get('/logout', (req, res, next) => {
+  req.session.destroy(err => res.redirect('/login'));
+});
+
 module.exports = router;
